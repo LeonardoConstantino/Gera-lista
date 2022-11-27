6f6faa008da11e3d42a77e3a6ec81fba42263c26
LeonardoConstantino <76814929+LeonardoConstantino@users.noreply.github.com>
2022-11-27 16:07:56 -0300

    Update script.js
    organização do código

--- src/script/script.js
+++ src/script/script.js
@@ -1,58 +1,32 @@
 const inputAdicionar = document.querySelector('[data-inputAdicionar]')
 const btnAdicionar = document.querySelector('[data-btnAdicionar]')
-const tagMain = document.querySelector('main')
 const inputPesquisar = document.querySelector('[data-inputPesquisar]')
 const btnDeleteAll = document.querySelector('[data-btnDeleteAll]')
 const ul = document.querySelector('[data-ul]')
 const checkboxEditar = document.querySelector('[data-checkboxEditar]')
-const checkboxsSelecionar = Array.from(document.querySelectorAll('[data-checkboxSelecionar]'))
 const checkboxSelectAll = document.querySelector('[data-checkboxSelectAll]')
+const fildsetTexto = document.querySelector('.form__texto')
 const checkboxnumero = document.querySelector('[data-checkboxnumero]')
 const textarea = document.querySelector('[data-textarea]')
+const btnReceber = document.querySelector('[data-btnReceber]')
+const btnCopiar = document.querySelector('[data-btnCopiar]')
 const btnAbreEnviar = document.querySelector('[data-btnAbreEnviar]')
-const inputTelefone = document.querySelector('[data-inputTelefone]')
 const btnEnviar = document.querySelector('[data-btnEnviar]')
+const btnNavegacao = document.querySelector('[data-btnNavegacao]')
 const modal = document.querySelector('[data-modal]')
+const btnFechaModal = document.querySelector('[data-btnFechaModal]')
 const dados = JSON.parse(localStorage.getItem('dadosLista')) || {
-    "itens": [/*{
-            "item": "item 1",
-            "checkado": false
-        },
-        {
-            "item": "item 2",
-            "checkado": false
-        },
-        {
-            "item": "item 3",
-            "checkado": false
-        },
-        {
-            "item": "atem 4",
-            "checkado": true
-        },
-        {
-            "item": "item 5",
-            "checkado": false
-        },
-        {
-            "item": "item 6",
-            "checkado": false
-        },
-        {
-            "item": "item 7",
-            "checkado": false
-        },*/
-    ]
+    "itens": []
 }
 const {
     itens
 } = dados
 let iniciou = true
-// console.log(item,checkado);
+let cron = null
 
-const moverParaTopo = () => {
+const moverParaTopo = (el, offset = 0) => {
     window.scrollTo({
-        top: tagMain.offsetTop - 12,
+        top: el.offsetTop + offset,
         behavior: 'smooth'
     })
 };
@@ -61,7 +35,10 @@ const salvaLocalStorage = (nome, item) => {
     localStorage.setItem(nome, JSON.stringify(item))
 }
 
-const capitalized = (str) => str[0].toUpperCase() + str.substr(1)
+const capitalized = (str) => {
+    if (!str) return "kkkkkk"
+    return str[0].toUpperCase() + str.substr(1)
+}
 
 const criarLista = () => {
     const filtro = itens.filter(item => item.checkado)
@@ -110,20 +87,23 @@ const setChekcboxIndefinido = () => {
 const criar = (e) => {
     e.preventDefault()
     const item = {
-        item: capitalized(inputAdicionar.value.toLocaleLowerCase()),
+        item: capitalized(inputAdicionar.value.toLowerCase().trim()),
         checkado: false
     }
     btnAdicionar.setAttribute("disabled", "")
     inputAdicionar.focus()
-    itens.push(item)
     inputAdicionar.value = ""
+    itens.push(item)
     salvaLocalStorage('dadosLista', dados)
     ler()
 }
 
 const ler = () => {
-    if(itens.length === 0) return
-    itens.sort((x, y) => x.item.toLowerCase().localeCompare(y.item.toLowerCase()))
+    if (itens.length === 0) return
+    itens.sort((x, y) => {
+        if (!x.item || !y.item) return
+        return x.item.toLowerCase().localeCompare(y.item.toLowerCase())
+    })
     ul.innerHTML = ""
     itens.forEach((item, index, array) => {
         const li = document.createElement("li")
@@ -190,9 +170,10 @@ const ler = () => {
         li.appendChild(divConteiner)
         ul.appendChild(li)
 
-        input.addEventListener('change', (e) => atualizaLista(e, item)) //nao ta pronto
+        input.addEventListener('change', (e) => atualizaLista(e, item))
         buttonEditar.addEventListener('click', (e) => {
             e.preventDefault()
+            const tagMain = document.querySelector('main')
             const liItem = e.target.parentNode.parentNode
             inputAdicionar.value = item.item
 
@@ -203,7 +184,7 @@ const ler = () => {
                 liItem.remove()
             }, 800)
             btnAdicionar.removeAttribute("disabled")
-            moverParaTopo()
+            moverParaTopo(tagMain, -12)
         })
         buttonApagar.addEventListener('click', (e) => {
             e.preventDefault()
@@ -219,14 +200,7 @@ const ler = () => {
     iniciou = false
 }
 
-ler()
-
-setChekcboxIndefinido()
-
-inputAdicionar.addEventListener('focus', moverParaTopo)
-
-inputAdicionar.addEventListener('input', (e) => {
-    e.preventDefault()
+const setDisabledBtnAdicionar = () => {
     const item = inputAdicionar.value
     if (!item) {
         btnAdicionar.setAttribute("disabled", "")
@@ -234,12 +208,36 @@ inputAdicionar.addEventListener('input', (e) => {
     }
     btnAdicionar.removeAttribute("disabled")
 
-})
+}
 
-btnAdicionar.addEventListener('click', (e) => criar(e))
+const setDisabledBtnReceber = () => {
+    if (!textarea.value) {
+        btnReceber.style.opacity = .1
+        btnReceber.setAttribute("disabled", "")
+        return
+    }
+    btnReceber.removeAttribute("disabled")
+    btnReceber.style.opacity = 1
+}
 
-checkboxEditar.addEventListener('change', (e) => {
+const receberLista = (e) => {
     e.preventDefault()
+    const novaLista = textarea.value.split(`\n`)
+
+    novaLista.forEach(item => {
+        const novoItem = {
+            item: capitalized(item.toLowerCase().trim()),
+            checkado: false
+        }
+        if (itens.includes(novoItem)) return
+        itens.push(novoItem)
+    })
+    textarea.value = ""
+    salvaLocalStorage('dadosLista', dados)
+    ler()
+}
+
+const seteModoEditar = () => {
     const allCheckbox = Array.from(document.querySelectorAll('[data-allCheckbox]'))
     const btnsEditar = Array.from(document.querySelectorAll('[data-btnEditar]'))
     const btnsDelete = Array.from(document.querySelectorAll('[data-btnDelete]'))
@@ -252,53 +250,166 @@ checkboxEditar.addEventListener('change', (e) => {
     }
     btns.forEach((btn) => btn.classList.add("display-none"))
     allCheckbox.forEach((Checkbox) => Checkbox.classList.remove("display-none"))
-})
+}
 
-inputPesquisar.addEventListener('input', (e)=>{
+const mostraElementosPesquisados = (e) => {
     const lis = Array.from(document.querySelectorAll('li'))
-    console.log(lis[0].children[0].innerText)
-    lis.forEach((li)=>{
+
+    lis.forEach((li) => {
         const termoPesquisado = e.target.value.toLowerCase()
         const textoLi = li.children[0].innerText.toLowerCase()
 
         li.classList.add("display-none")
-        
+
         if (textoLi.includes(termoPesquisado)) {
             li.classList.remove("display-none")
-            
+
         }
     })
-})
+}
 
-checkboxnumero.addEventListener('change', criarLista)
-
-btnDeleteAll.addEventListener('click', (e) => {
+const apagarTudoOuCancelar = (e) => {
     e.preventDefault()
-    const lis = document.querySelectorAll('li')
-    lis.forEach((li, index) => {
-        li.style.animationDelay = `.${1+index}s`
-        li.classList.add("animaApagar")
+    let contador = 4
+
+    if (btnDeleteAll.innerText.includes('Cancelar')) {
+        ul.classList.remove("triste")
+
+        btnDeleteAll.innerHTML = `
+        <span>Apagar tudo</span>
+        <i class="material-symbols-outlined">delete</i>
+        `
+
+        clearInterval(cron)
+        return
+    }
+
+    btnDeleteAll.innerHTML = `
+        <span>Cancelar... 5</span>
+    `
+
+    ul.classList.add("triste")
+
+    cron = setInterval(() => {
+        contador--
+        btnDeleteAll.innerHTML = `
+            <span>Cancelar... ${contador}</span>
+        `
+        if (contador === -1) {
+            clearInterval(cron)
+            ul.classList.remove("triste")
+
+            btnDeleteAll.innerHTML = `
+            <span>Apagar tudo</span>
+            <i class="material-symbols-outlined">delete</i>
+            `
+
+            const lis = document.querySelectorAll('li')
+            lis.forEach((li, index) => {
+                li.style.animationDelay = `.${1+index}s`
+                li.classList.add("animaApagar")
+
+                setTimeout(() => {
+                    li.remove()
+                }, 500)
+            })
+
+            itens.length = 0
+            salvaLocalStorage("dadosLista", dados)
+        }
+    }, 1000)
+}
+
+const abrirModalEnviar = (e) => {
+    e.preventDefault()
+    if (!textarea.value) {
+        textarea.value = 'Você ainda nao tem uma lista para enviar.'
         setTimeout(() => {
-            li.remove()
-        }, 500)
-    })
-    itens.length = 0
-    salvaLocalStorage("dadosLista", dados)
-})
-
-btnAbreEnviar.addEventListener('click', (e) => {
-    e.preventDefault()
+            textarea.value = ''
+        }, 5000)
+    }
     modal.classList.remove("display-none")
     modal.showModal()
-})
+}
 
-btnEnviar.addEventListener('click', (e) => {
+const enviarParaWhatsApp = (e) => {
+    e.preventDefault()
+    const inputTelefone = document.querySelector('[data-inputTelefone]')
+
+    const titulo = 'Gera lista\n[̲̅b̲̅y̲̅l̲̅є̲̅σ̲̅]\n\n'
+    const linkApi = 'https://api.whatsapp.com/send?phone=55'
+    const texto = `&text=${titulo}${textarea.value}`
+    const pattern = /^\(?\d{2}\)?[\s-]?[\s9]\d{4}-?\d{4}$/i;
+    const telefone = inputTelefone.value.match(pattern)[0]
+    const linkCompleto = linkApi + telefone + encodeURI(texto)
+
+    const mostrarMsgErroTel = () => {
+        const msgErroTel = document.querySelector('[data-msgErroTel]')
+
+        if (!msgErroTel.classList.contains("display-none")) return
+        
+        msgErroTel.classList.remove("display-none")
+        setTimeout(() => {
+            msgErroTel.classList.add("display-none")
+        }, 1000)
+
+    }
+
+    if (!inputTelefone.value.match(pattern)) {
+        mostrarMsgErroTel()
+        return
+    }
+
+    window.open(linkCompleto)
+
+    modal.close()
+    modal.classList.add("display-none")
+}
+
+const fecharModal = (e) => {
     e.preventDefault()
     modal.close()
     modal.classList.add("display-none")
-})
+}
 
-checkboxSelectAll.addEventListener('change', () => {
+const enviarParaAreaDeTranferencia = (e) => {
+    e.preventDefault()
+    if (!textarea.value) {
+        textarea.value = 'Você ainda nao tem uma lista para copiar.'
+        setTimeout(() => {
+            textarea.value = ''
+        }, 5000)
+    }
+
+    const titulo = 'Gera lista\n[̲̅b̲̅y̲̅l̲̅є̲̅σ̲̅]\n\n'
+    navigator.clipboard.writeText(titulo + textarea.value)
+    textarea.value = 'Lista enviada para area de tranferencia'
+    setTimeout(() => {
+        textarea.value = ''
+    }, 5000)
+}
+
+const setSubirOuDescer = (e) => {
+    e.preventDefault()
+    if (btnNavegacao.children[0].innerText === 'arrow_downward') {
+        moverParaTopo(fildsetTexto, 12)
+        return
+    }
+    moverParaTopo(inputPesquisar, -12)
+}
+
+const setSetaSubirOuDescer = () => {
+    const tamanhoDaTela = window.innerHeight
+    const distanciaDoTopo = btnNavegacao.offsetTop
+
+    if (distanciaDoTopo - 150 > tamanhoDaTela) {
+        btnNavegacao.children[0].innerText = 'arrow_upward'
+        return
+    }
+    btnNavegacao.children[0].innerText = 'arrow_downward'
+}
+
+const setCheckedOuNaoTodosCheckbox = () => {
     const labelSelectAll = document.querySelector('[data-labelSelectAll]')
     const checkboxsSelecionar = Array.from(document.querySelectorAll('[data-checkboxSelecionar]'))
 
@@ -316,23 +427,66 @@ checkboxSelectAll.addEventListener('change', () => {
     checkboxsSelecionar.forEach((checkbox) => checkbox.checked = false)
     salvaLocalStorage("dadosLista", dados)
     criarLista()
+}
+
+ler()
+
+setChekcboxIndefinido()
+
+inputAdicionar.addEventListener('focus', () => {
+    moverParaTopo(inputAdicionar, 4)
 })
 
+inputPesquisar.addEventListener('focus', () => {
+    moverParaTopo(inputPesquisar, -12)
+})
 
+textarea.addEventListener('focus', () => {
+    moverParaTopo(fildsetTexto, 12)
+})
 
+inputAdicionar.addEventListener('input', setDisabledBtnAdicionar)
 
-/* 
-<li class="form__list-li">
-    <p>Item 1</p>
-    <div>
-        <div>
-            <label class="checkbox-estilizado form__list-li-checkbox" data-liCheckbox>
-                <input type="checkbox" data-checkboxSelecionar>
-                <span class="checkmark"></span>
-            </label>
-        </div>
-        <button class="material-symbols-outlined form__list-li-btn form__list-li-btn--editar display-none" data-btnEditar title="Editar">edit_note</button>
-        <button class="material-symbols-outlined form__list-li-btn form__list-li-btn--delete display-none" data-btnDelete title="Apagar">delete</button>
-    </div>
-</li>
-*/
+textarea.addEventListener('input', setDisabledBtnReceber)
+
+btnReceber.addEventListener('click', e => {
+    receberLista(e)
+})
+
+btnAdicionar.addEventListener('click', e => criar(e))
+
+checkboxEditar.addEventListener('change', seteModoEditar)
+
+inputPesquisar.addEventListener('input', e => {
+    mostraElementosPesquisados(e)
+})
+
+checkboxnumero.addEventListener('change', criarLista)
+
+btnDeleteAll.addEventListener('click', e => {
+    apagarTudoOuCancelar(e)
+})
+
+btnAbreEnviar.addEventListener('click', e => {
+    abrirModalEnviar(e)
+})
+
+btnEnviar.addEventListener('click', e => {
+    enviarParaWhatsApp(e)
+})
+
+btnFechaModal.addEventListener('click', e => {
+    fecharModal(e)
+})
+
+btnCopiar.addEventListener('click', e => {
+    enviarParaAreaDeTranferencia(e)
+})
+
+btnNavegacao.addEventListener('click', e => {
+    setSubirOuDescer(e)
+})
+
+window.addEventListener('scroll', setSetaSubirOuDescer)
+
+checkboxSelectAll.addEventListener('change', setCheckedOuNaoTodosCheckbox)
